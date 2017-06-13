3e0b2849a6c80c0f41ac5c9867cdd89ca45fa18e
George Hunt <georgejhunt@gmail.com>
2017-06-13 12:16:54 -0700

    not there on release notes

--- roles/httpd/templates/refresh-wiki-docs.sh
+++ roles/httpd/templates/refresh-wiki-docs.sh
@@ -32,7 +32,7 @@ for f in `ls /tmp/${WIKI}`; do
     sed -i -r "/.*#.*/! s|$REPO/$REPONAME/wiki/(.*)\">|./\1.html\">|" $OUTPUT/$FTRIMMED.html
     sed -i -e "s|http://schoolserver.org/faq|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
     sed -i -e "s|http://wiki.laptop.org/go/IIAB/FAQ|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
-    sed -i -e "s|http://wiki.laptop.org/go/XS_Community_Edition/FAQ.html|/info/html/FAQ|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|http://wiki.laptop.org/go/XS_Community_Edition/FAQ|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
     sed -i -e "s|http://FAQ.IIAB.IO/info/html/FAQ|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
     sed -i -e "s|https://github.com/xsce/blob/release-6.2/\(.*\)\">|./\1.html\">|"  $OUTPUT/$FTRIMMED.html
 done
@@ -48,13 +48,15 @@ lynx -reload -source https://github.com/XSCE/xsce/wiki/IIAB-6.2-Release-Notes>  
 lynx -reload -source https://github.com/XSCE/blob/release-6.2/Release-Notes6.0>  $OUTPUT/ReleaseNotes6.0.md
 lynx -reload -source https://github.com/XSCE/blob/release-6.2/Release-Notes6.1>  $OUTPUT/ReleaseNotes6.1.md
 
-for f in `ls $OUTPUT/*Release*.md`; do
+pushd $OUTPUT
+for f in `ls *Release*.md`; do
 #    FTRIMMED=${f%.md}
-    FTRIMMED=${f:2}
-    pandoc -s $f -o  $WWWROOT$TARGET_URL$FTRIMMED.html
+    FTRIMMED=${f:0:-3}
+    pandoc -s $f -o  $WWWROOT$TARGET_URL/$FTRIMMED.html
     # make links refer to local directory
-    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|"  $WWWROOT$TARGET_URL$FTRIMMED.html
+    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|"  $WWWROOT$TARGET_URL/$FTRIMMED.html
 done
+popd
 
 #rm -rf $INPUT
 #rm -rf $OUTPUT
