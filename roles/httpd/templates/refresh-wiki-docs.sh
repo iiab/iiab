5f46cdac0383abb00e5ec2547e9c724b45a62b5b
George Hunt <georgejhunt@gmail.com>
2017-06-13 11:50:07 -0700

    release notes still not working

--- roles/httpd/templates/refresh-wiki-docs.sh
+++ roles/httpd/templates/refresh-wiki-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-# pull down repo wiki, and imbed in docs subdirectory
-
+# pull down repo wiki, and use to create offline docs
+set -e
 source /etc/iiab/iiab.env
 REPONAME=iiab
 REPO=https://github.com/iiab
@@ -18,39 +18,37 @@ rm -rf $INPUT
 rm -rf $OUTPUT
 mkdir -p $INPUT
 mkdir -p $OUTPUT
+mkdir -p $WWWROOT$TARGET_URL/html
 
 git clone $REPO/$REPONAME.wiki.git $INPUT
 
-mkdir -p $WWWROOT$TARGET_URL/html
-
-# To Do find more links to rewrite, especially after moving from iiab to iiab
+# To Do find more links to rewrite, especially after moving from xsce to iiab
 for f in `ls /tmp/${WIKI}`; do
     FTRIMMED=${f%.md}
     if [ $FTRIMMED = "Home" ]; then FTRIMMED=index;fi
     pandoc -s /tmp/${WIKI}/$f -o $OUTPUT/$FTRIMMED.html
     # make links refer to local directory
-    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">|" $OUTPUT/$FTRIMMED.html
-    sed -i -e "s|http://schoolserver.org/faq|/info/html/FAQ|" $OUTPUT/$FTRIMMED.html
-    sed -i -e "s|$REPO/$REPONAME/blob/release-.*/\(.*\)\">|./\1.html\">|"  $OUTPUT/$FTRIMMED.html
+    sed -i -r "/.*#.*/ s|$REPO/$REPONAME/wiki/(.*)(#.*)\">|./\1.html\2\">|" $OUTPUT/$FTRIMMED.html
+    sed -i -r "/.*#.*/! s|$REPO/$REPONAME/wiki/(.*)\">|./\1.html\">|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|http://schoolserver.org/faq|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|http://wiki.laptop.org/go/IIAB/FAQ|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|http://wiki.laptop.org/go/XS_Community_Edition/FAQ.html|/info/html/FAQ|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|http://FAQ.IIAB.IO/info/html/FAQ|/info/html/FAQ.html|" $OUTPUT/$FTRIMMED.html
+    sed -i -e "s|https://github.com/xsce/blob/release-6.2/\(.*\)\">|./\1.html\">|"  $OUTPUT/$FTRIMMED.html
 done
 
 rsync -av $OUTPUT/ $WWWROOT$TARGET_URL
 
 # copy the faq and other things
-lynx -reload -source http://wiki.laptop.org/go/XS_Community_Edition/FAQ >  $WWWROOT$TARGET_URL/html/FAQ
-lynx -reload -source http://wiki.laptop.org/go/XS_Community_Edition/Security >  $WWWROOT$TARGET_URL/html/Security
+lynx -reload -source http://wiki.laptop.org/go/XS_Community_Edition/FAQ >  $WWWROOT$TARGET_URL/html/FAQ.html
+lynx -reload -source http://wiki.laptop.org/go/XS_Community_Edition/Security >  $WWWROOT$TARGET_URL/html/Security.html
 lynx -reload -source http://wiki.laptop.org/go/XS_Community_Edition/local_vars.yml >  $WWWROOT$TARGET_URL/html/local_vars.yml
-
-# fetch the embedded help pages from the admin console
-#for f in `ls ../roles/iiab-admin/files/console/help`; do
-#    FTRIMMED=${f%.rst}
-#    pandoc -s ../roles/iiab-admin/files/console/help/$f -o ../docs/html/offline-help/$FTRIMMED.html
-#    # make links refer to local directory
-#    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|" ../docs/html/$FTRIMMED.html
-#done
-
 # fetch the recent release notes
-for f in `ls ../Release*`; do
+lynx -reload -source https://github.com/XSCE/xsce/wiki/IIAB-6.2-Release-Notes>  $OUTPUT/IIAB-6.2-Release-Notes.md
+lynx -reload -source https://github.com/XSCE/blob/release-6.2/Release-Notes6.0>  $OUTPUT/ReleaseNotes6.0.md
+lynx -reload -source https://github.com/XSCE/blob/release-6.2/Release-Notes6.1>  $OUTPUT/ReleaseNotes6.1.md
+
+for f in `ls $OUTPUT/*Release*.md`; do
 #    FTRIMMED=${f%.md}
     FTRIMMED=${f:2}
     pandoc -s $f -o  $WWWROOT$TARGET_URL$FTRIMMED.html
@@ -58,8 +56,8 @@ for f in `ls ../Release*`; do
     sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|"  $WWWROOT$TARGET_URL$FTRIMMED.html
 done
 
-rm -rf $INPUT
-rm -rf $OUTPUT
+#rm -rf $INPUT
+#rm -rf $OUTPUT
 
 popd
 
