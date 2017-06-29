edb4a2a1d25e83c2d053932f72454c7012b2c4e8
George Hunt <georgejhunt@gmail.com>
2017-06-29 12:48:08 -0700

    wrinkles in centos

--- first.sh
+++ first.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # recognize OS, and other first things
 OS=`grep ^ID= /etc/*elease | cut -d= -f2`
+# centos encloses the ID in quites -- clip them
+OS=${OS##\"}
+OS=${OS%%\"}
 export OS
 VERSION_ID=`grep VERSION_ID /etc/*elease | cut -d= -f2`
 VER=${VERSION_ID%%\"}
 VER=${VER##\"}
+# ubuntu currently gives us 16.04, use major number
 VER=${VER%%.*}
 OS_CONTEXT=$OS-$VER
 export OS_CONTEXT
