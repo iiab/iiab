b325eab2d6b0ae3382dec6fa498214cfb3429f56
Jerry Vonau <jvonau3@gmail.com>
2017-11-19 14:44:52 -0600

    remove echos and add notes

--- scripts/calibre312.sh
+++ scripts/calibre312.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-echo "install stock calibre-bin"
-apt-get -y install calibre-bin dirmngr
+export DEBIAN_FRONTEND=noninteractive
+# drags in stock desktop dependencies without too much from testing below
+apt -y install calibre-bin dirmngr
 
-echo "installing debian key"
+# updates calibre-bin to version 3.10 from testing
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
-
-echo "enabling testing"
 echo "deb http://deb.debian.org/debian testing main" > /etc/apt/sources.list.d/debian-testing.list
-apt-get update
-
-echo "updating dependencies"
-apt-get -y install libqt5core5a  python-lxml
+apt update
+apt -y install libqt5core5a  python-lxml
 rm /etc/apt/sources.list.d/debian-testing.list
 
-echo "enabling unstable"
+# updates to calibre & calibre-bin to 3.12 from unstable
 echo "deb http://deb.debian.org/debian unstable main" > /etc/apt/sources.list.d/debian-unstable.list
-apt-get update
-
-echo "installing calibre 3.12"
-apt-get -y install calibre
+apt update
+apt -y install calibre
 rm /etc/apt/sources.list.d/debian-unstable.list
 
-echo "disabled testing and unstable repos"
-apt-get update
-
-echo "DONE"
+# clears the cache of testing and unstable
+apt update
