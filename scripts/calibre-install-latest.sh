178960a0883f1cd89c295de585418b52e155d0d3
A Holt <holta@users.noreply.github.com>
2017-11-20 03:53:27 -0500

    Update calibre-install-latest.sh

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-# drags in stock desktop dependencies without too much from testing below
+# Drags in stock desktop dependencies without too much from testing below
 apt -y install calibre-bin dirmngr
 
-# updates calibre-bin to version 3.10 from testing
+# Updates calibre-bin to version 3.10 from testing
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
-echo "deb http://deb.debian.org/debian testing main" > /etc/apt/sources.list.d/debian-testing.list
+echo "deb http://deb.debian.org/debian testing main" >> /etc/apt/sources.list.d/debian-testing.list
 apt update
 apt -y install libqt5core5a python-lxml calibre
-rm /etc/apt/sources.list.d/debian-testing.list
+# Remove last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
+sed -i '$ d' /etc/apt/sources.list.d/debian-testing.list
 
-# updates to calibre & calibre-bin to 3.12 from unstable
-echo "deb http://deb.debian.org/debian unstable main" > /etc/apt/sources.list.d/debian-unstable.list
+# Updates to calibre & calibre-bin to 3.12 from unstable
+echo "deb http://deb.debian.org/debian unstable main" >> /etc/apt/sources.list.d/debian-unstable.list
 apt update
 apt -y install calibre
-rm /etc/apt/sources.list.d/debian-unstable.list
+# Remove last line, safer than: rm /etc/apt/sources.list.d/debian-unstable.list
+sed -i '$ d' /etc/apt/sources.list.d/debian-unstable.list
 
-# clears the cache of testing and unstable
+# Clears the cache of testing and unstable
 apt update
