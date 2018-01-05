68c966a8a7c8b4e5ddc4b7dd81cb616b6e72c39d
A Holt <holta@users.noreply.github.com>
2018-01-04 22:46:42 -0500

    wlan0 promiscuous...only if rpi

--- roles/2-common/templates/iiab-startup.sh
+++ roles/2-common/templates/iiab-startup.sh
@@ -5,8 +5,8 @@ if [ ! -f /etc/iiab/uuid ]; then
    uuidgen > /etc/iiab/uuid
 fi
 
-# Experimental/Temporary WiFi workaround:
+# Experimental/Temporary workaround for WiFi "10SEC disease"
 # https://github.com/iiab/iiab/issues/638#issuecomment-355455454
-ip link set dev wlan0 promisc on
+if grep -qi raspbian /etc/*release; then ip link set dev wlan0 promisc on; fi
 
 exit 0
