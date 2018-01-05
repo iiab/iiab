5b4c09572cde3c4c3f833da7b264655a83d33491
A Holt <holta@users.noreply.github.com>
2018-01-04 21:44:38 -0500

    promiscuous mode on boot, for all IIAB's ??

--- roles/2-common/templates/iiab-startup.sh
+++ roles/2-common/templates/iiab-startup.sh
@@ -4,5 +4,9 @@
 if [ ! -f /etc/iiab/uuid ]; then
    uuidgen > /etc/iiab/uuid
 fi
-exit 0
 
+# Experimental/Temporary WiFi workaround:
+# https://github.com/iiab/iiab/issues/638#issuecomment-355455454
+ip link set dev wlan0 promisc on
+
+exit 0
