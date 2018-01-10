3ccccbd9bcd20266d0e287c6930d3f9184c949cf
A Holt <holta@users.noreply.github.com>
2018-01-10 17:09:09 -0500

    Clarify how calibre-install-latest.sh works in comments?

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Drags in stock desktop dependencies without too much from testing below
 apt -y install calibre-bin dirmngr
 
-# Updates calibre-bin to version 3.10 from testing
+# Updates calibre-bin to version 3.10 or 3.14 or ... from testing (SEE http://archive.raspbian.org/raspbian/pool/main/c/calibre/ ?)
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
 echo "deb http://deb.debian.org/debian testing main" >> /etc/apt/sources.list.d/debian-testing.list
 apt update
@@ -15,7 +15,7 @@ apt -y install libqt5core5a python-lxml calibre
 # Remove last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
 sed -i '$ d' /etc/apt/sources.list.d/debian-testing.list
 
-# Updates to calibre & calibre-bin to 3.12 from unstable
+# Updates to calibre & calibre-bin to "very latest" 3.x from unstable
 echo "deb http://deb.debian.org/debian unstable main" >> /etc/apt/sources.list.d/debian-unstable.list
 apt update
 apt -y install calibre
