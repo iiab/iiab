1178c452a7cf47e7f9f7f9e78c3205085f292f4d
A Holt <holta@users.noreply.github.com>
2018-01-11 13:51:22 -0500

    backslash not needed (in bash conditional)

--- roles/2-common/templates/iiab-startup.sh
+++ roles/2-common/templates/iiab-startup.sh
@@ -9,7 +9,7 @@ fi
 # Set wlan0 to promiscuous on boot if needed as gateway (i.e. AP's OFF)
 # Scripts iiab-hotspot-on + iiab-hotspot-off SHOULD toggle this boot flag!
 # https://github.com/iiab/iiab/issues/638#issuecomment-355455454
-if [[ $(grep -i raspbian /etc/*release) && \
+if [[ $(grep -i raspbian /etc/*release) &&
         #($(grep "hostapd_enabled = False" /etc/iiab/config_vars.yml) ||
             #((! $(grep "hostapd_enabled = True" /etc/iiab/config_vars.yml)) &&
                  $(grep "^hostapd_enabled = False" /etc/iiab/iiab.ini) ]];
