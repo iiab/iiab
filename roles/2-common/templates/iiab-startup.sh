c438c13fb1c15b4a0e0cd74f092027d92db57416
A Holt <holta@users.noreply.github.com>
2018-01-10 22:42:09 -0500

    Enable Promiscuous WiFi on boot IFF AP's OFF

--- roles/2-common/templates/iiab-startup.sh
+++ roles/2-common/templates/iiab-startup.sh
@@ -5,8 +5,17 @@ if [ ! -f /etc/iiab/uuid ]; then
    uuidgen > /etc/iiab/uuid
 fi
 
-# Experimental/Temporary workaround for WiFi "10SEC disease"
+# Temporary promiscuous-mode workaround for WiFi "10SEC disease"
 # https://github.com/iiab/iiab/issues/638#issuecomment-355455454
-if grep -qi raspbian /etc/*release; then ip link set dev wlan0 promisc on; fi
+if [[ $(grep -i raspbian /etc/*release) &&
+         ($(grep "hostapd_enabled = False" /etc/iiab/config_vars.yml) ||
+             ((! $(grep "hostapd_enabled = True" /etc/iiab/config_vars.yml)) &&
+                 $(grep "hostapd_enabled = False" /etc/iiab/iiab.ini)
+             )
+         )
+   ]];
+then
+    ip link set dev wlan0 promisc on
+fi
 
 exit 0
