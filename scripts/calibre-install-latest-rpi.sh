8dcfb7e7ad4ff30cbb0bb0456c79b54668d0dfe7
A Holt <holta@users.noreply.github.com>
2018-04-21 19:58:26 -0400

    Update comment in calibre-install-latest-rpi.sh

--- scripts/calibre-install-latest-rpi.sh
+++ scripts/calibre-install-latest-rpi.sh
@@ -2,13 +2,14 @@
 
 # Thanks to Jerry Vonau (https://github.com/jvonau) who made
 # this critical breakthrough (Calibre 3.x on Raspbian) possible!
-# The latest available is 3.20 available from testing
+# The latest available is 3.21 available from testing
 # https://packages.debian.org/search?keywords=calibre
-# (SEE http://archive.raspbian.org/raspbian/pool/main/c/calibre/ ?)
-# might break future updates, you have been warned
+# (SEE http://raspbian.raspberrypi.org/raspbian/pool/main/c/calibre/ 
+#  OR http://archive.raspbian.org/raspbian/pool/main/c/calibre/ ?)
+# Might break future updates, you have been warned.
 
 export DEBIAN_FRONTEND=noninteractive
-# Updates calibre calibre-bin to version 3.20 or ... from testing
+# Updates calibre calibre-bin to version 3.21 or ... from testing
 echo "deb http://raspbian.raspberrypi.org/raspbian/ testing main" > /etc/apt/sources.list.d/rpi-testing.list
 apt update
 apt -y install calibre calibre-bin
