463877e105ef9aef1a3150af1edee6ccf95626be
A Holt <holta@users.noreply.github.com>
2018-05-10 23:59:58 -0400

    Update calibre-install-latest.sh

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -15,12 +15,12 @@
 export DEBIAN_FRONTEND=noninteractive
 # Drags in stock desktop dependencies without too much from testing below
 apt -y install calibre-bin dirmngr
-# Prepare to update to latest from testing
+# Prepares to update to latest from testing
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
 echo "deb http://deb.debian.org/debian testing main" >> /etc/apt/sources.list.d/debian-testing.list
 apt update
 apt -y install libqt5core5a python-lxml calibre
-# Remove last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
+# Removes last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
 sed -i '$ d' /etc/apt/sources.list.d/debian-testing.list
 # Clears the cache of testing
 apt update
