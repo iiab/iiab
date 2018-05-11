2af5e8781a99ad1c20c65691a42af88a3732b6e1
A Holt <holta@users.noreply.github.com>
2018-05-10 23:59:37 -0400

    Update calibre-install-latest-rpi.sh

--- scripts/calibre-install-latest-rpi.sh
+++ scripts/calibre-install-latest-rpi.sh
@@ -13,11 +13,11 @@
 # Might break future updates; you have been warned.
 
 export DEBIAN_FRONTEND=noninteractive
-# Prepare to update to latest from testing
+# Prepares to update to latest from testing
 echo "deb http://raspbian.raspberrypi.org/raspbian/ testing main" > /etc/apt/sources.list.d/rpi-testing.list
 apt update
 apt -y install calibre calibre-bin
-# Remove last line, safer than: rm /etc/apt/sources.list.d/rpi-testing.list
+# Removes last line, safer than: rm /etc/apt/sources.list.d/rpi-testing.list
 sed -i '$ d' /etc/apt/sources.list.d/rpi-testing.list
 # Clears the cache of testing
 apt update
