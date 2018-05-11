81ce4135953360a7d71e4f04ec12a42dcfb9a38e
A Holt <holta@users.noreply.github.com>
2018-05-10 21:25:48 -0400

    Update calibre-install-latest.sh

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -4,15 +4,18 @@
 # breakthrough possible!
 #
 # Calibre 3.23 is the latest available from testing as of 2018-05-10:
-# https://packages.debian.org/search?keywords=calibre
-# (SEE ALSO http://deb.debian.org/debian/pool/main/c/calibre/ ?)
+#
+#   https://packages.debian.org/search?keywords=calibre
+#   http://deb.debian.org/debian/pool/main/c/calibre/
+#   http://raspbian.raspberrypi.org/raspbian/pool/main/c/calibre/
+#   http://archive.raspbian.org/raspbian/pool/main/c/calibre/
 #
 # Might break future updates; you have been warned.
 
 export DEBIAN_FRONTEND=noninteractive
 # Drags in stock desktop dependencies without too much from testing below
 apt -y install calibre-bin dirmngr
-# Updates calibre calibre-bin to version 3.20 or ... from testing
+# Prepare to update to latest from testing
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
 echo "deb http://deb.debian.org/debian testing main" >> /etc/apt/sources.list.d/debian-testing.list
 apt update
