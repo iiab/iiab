4f3f33fa3d6b81123641aa1bfd9e94a554b69fbb
A Holt <holta@users.noreply.github.com>
2018-05-10 21:32:40 -0400

    Update calibre-install-latest.sh

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -22,4 +22,5 @@ apt update
 apt -y install libqt5core5a python-lxml calibre
 # Remove last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
 sed -i '$ d' /etc/apt/sources.list.d/debian-testing.list
+# Clears the cache of testing
 apt update
