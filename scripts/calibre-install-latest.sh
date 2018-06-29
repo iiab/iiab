6116c2f3737fd3afead2763c43eb657a2db09e1c
A Holt <holta@users.noreply.github.com>
2018-06-28 22:13:52 -0400

    even better cleanup for Calibre 3.26+

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -3,7 +3,7 @@
 # Thanks to Jerry Vonau (https://github.com/jvonau) who made this critical
 # breakthrough possible!
 #
-# Calibre 3.25 is the latest available from testing as of 2018-06-10:
+# Calibre 3.26.x is the latest available from testing as of 2018-06-28:
 #
 #   https://packages.debian.org/search?keywords=calibre
 #   http://deb.debian.org/debian/pool/main/c/calibre/
