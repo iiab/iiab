ab3e06bae8d40bc947b32e261710e1612eaa61ab
A Holt <holta@users.noreply.github.com>
2018-08-23 13:57:37 -0400

    Update calibre-install-latest.sh

--- scripts/calibre-install-latest.sh
+++ scripts/calibre-install-latest.sh
@@ -22,9 +22,10 @@ apt -y install dirmngr
 apt-key adv --recv-key --keyserver keyserver.ubuntu.com 7638D0442B90D010
 echo "deb http://deb.debian.org/debian testing main" > /etc/apt/sources.list.d/debian-testing.list
 apt update
-apt -y install libqt5core5a python-lxml calibre calibre-bin
+#apt -y install libqt5core5a python-lxml calibre calibre-bin
+apt -y install calibre calibre-bin
 ## Removes last line, safer than: rm /etc/apt/sources.list.d/debian-testing.list
 #sed -i '$ d' /etc/apt/sources.list.d/debian-testing.list
 rm /etc/apt/sources.list.d/debian-testing.list
-# Clears the cache of testing
+# Clears the cache of debian/testing
 apt update
