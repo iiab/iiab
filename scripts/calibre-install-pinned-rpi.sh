ee9338cdba334136696ec52de36d71214a8589a1
A Holt <holta@users.noreply.github.com>
2018-08-30 13:24:08 -0400

    Update calibre-install-pinned-rpi.sh

--- scripts/calibre-install-pinned-rpi.sh
+++ scripts/calibre-install-pinned-rpi.sh
@@ -10,6 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Prepares to update to latest from raspbian testing
 echo "deb http://raspbian.raspberrypi.org/raspbian/ testing main" > /etc/apt/sources.list.d/rpi-testing.list
 apt update
+# @jvonau explains that stale .deb's should be avoided as: "apt will pick the highest version to install after churning a bit..."
 apt -y install /opt/iiab/downloads/calibre*.deb
 #sed -i '$ d' /etc/apt/sources.list.d/rpi-testing.list    # Removes last line
 rm /etc/apt/sources.list.d/rpi-testing.list
