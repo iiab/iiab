a15a26808f9a851962d3f5400e67bc363eb013f8
Jerry Vonau <jvonau3@gmail.com>
2018-10-09 13:24:07 -0500

    split out enable -- merge conflict

--- scripts/calibre-install-pinned-rpi.sh
+++ scripts/calibre-install-pinned-rpi.sh
@@ -12,6 +12,7 @@ echo "deb http://raspbian.raspberrypi.org/raspbian/ testing main" > /etc/apt/sou
 apt update
 # WARNING: you MUST remove old .deb's from /opt/iiab/downloads if upgrading Calibre in this way, SEE: http://FAQ.IIAB.IO "Can I upgrade or reinstall server apps?"
 apt -y install sqlite3    # Appears no longer nec as of 2018-10-23.  Was required in Sept 2018 as workaround for https://github.com/iiab/iiab/issues/1139 that blocked install of Admin Console
+apt -y install /opt/iiab/downloads/calibre*.deb
 #sed -i '$ d' /etc/apt/sources.list.d/rpi-testing.list    # Removes last line
 rm /etc/apt/sources.list.d/rpi-testing.list
 # Clears the cache of rpi/testing
