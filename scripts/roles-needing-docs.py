4b43f4653085ad3d63b13c0af1435678d7e0ccb7
A Holt <holta@users.noreply.github.com>
2018-10-09 15:10:23 -0400

    Update roles-needing-docs.py

--- scripts/roles-needing-docs.py
+++ scripts/roles-needing-docs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 '''
-This script checks every role in the project and prints its name to stdout if 
-the role directory does not contain a README file and it is not listed in 
-scripts/docs-ignore.
+This script checks every role in the (Internet-in-a-Box) project and prints its
+name to stdout if (1) the role directory does not contain a README file, and
+(2) the role is not listed in /opt/iiab/iiab/unmaintained-roles.txt
 
 For ease of use, you can pipe the output of this script to a file or to a 
 clipboard utility (e.g. pbcopy on macOS, xclip on Linux).
@@ -16,7 +16,9 @@ from glob import glob
 def included_roles():
     all_roles = set(os.listdir("/opt/iiab/iiab/roles"))
     excluded_roles = \
-        map(str.rstrip, open(make_path("/opt/iiab/iiab/scripts", "/opt/iiab/iiab/scripts/docs-ignore")))
+        map(str.rstrip,
+            open(make_path("/opt/iiab/iiab/scripts",
+                           "/opt/iiab/iiab/unmaintained-roles.txt")))
     included_roles = list(all_roles.difference(excluded_roles))
     included_roles.sort()
     return included_roles
