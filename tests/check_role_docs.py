6a046ed7891310cfdf0e6daaed2f677e0e361404
Aidan Fitzgerald <acf67@cornell.edu>
2018-10-09 12:19:30 -0400

    Create script to check roles for missing documentation

---
+++ tests/check_role_docs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+This script checks every role in the project and prints its name to stdout if 
+the role directory does not contain a README file.
+
+For ease of use, you can pipe the output of this script to a file or to a 
+clipboard utility (e.g. pbcopy on macOS, xclip on Linux).
+'''
+
+import os, glob
+
+for role in os.listdir("roles"):
+    readme_glob = os.path.join("roles", role, "README.*")
+    if not glob.glob(readme_glob):
+        print(role)
