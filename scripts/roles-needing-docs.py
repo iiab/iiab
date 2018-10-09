f915b5ee224b3081dea8a28730f261c36aeb7cdc
A Holt <holta@users.noreply.github.com>
2018-10-09 14:57:23 -0400

    Update and rename roles_needing_docs.py to roles-needing-docs.py

--- scripts/roles-needing-docs.py
+++ scripts/roles-needing-docs.py
@@ -3,7 +3,7 @@
 '''
 This script checks every role in the project and prints its name to stdout if 
 the role directory does not contain a README file and it is not listed in 
-scripts/docs_ignore.
+scripts/docs-ignore.
 
 For ease of use, you can pipe the output of this script to a file or to a 
 clipboard utility (e.g. pbcopy on macOS, xclip on Linux).
@@ -14,14 +14,14 @@ from os.path import join as make_path
 from glob import glob
 
 def included_roles():
-    all_roles = set(os.listdir("roles"))
+    all_roles = set(os.listdir("/opt/iiab/iiab/roles"))
     excluded_roles = \
-        map(str.rstrip, open(make_path("scripts", "docs_ignore")))
+        map(str.rstrip, open(make_path("/opt/iiab/iiab/scripts", "/opt/iiab/iiab/scripts/docs-ignore")))
     included_roles = list(all_roles.difference(excluded_roles))
     included_roles.sort()
     return included_roles
 
 for role in included_roles():
-    readme = make_path("roles", role, "README.*")
+    readme = make_path("/opt/iiab/iiab/roles", role, "README.*")
     if not glob(readme):
         print(role)
