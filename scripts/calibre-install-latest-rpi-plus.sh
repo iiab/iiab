3a28aa6cf419167fd78e89a13380786f49ee3dd3
A Holt <holta@users.noreply.github.com>
2018-10-23 18:28:31 -0400

    Update calibre-install-latest-rpi-plus.sh

--- scripts/calibre-install-latest-rpi-plus.sh
+++ scripts/calibre-install-latest-rpi-plus.sh
@@ -27,8 +27,7 @@ rm /etc/apt/sources.list.d/debian-testing.list
 # Prepares to update from raspbian testing
 echo "deb http://raspbian.raspberrypi.org/raspbian/ testing main" > /etc/apt/sources.list.d/rpi-testing.list
 apt update
-apt -y install sqlite3    # workaround for https://github.com/iiab/iiab/issues/1139 that blocked install of Admin Console
-apt -y install calibre calibre-bin
+apt -y install sqlite3    # Appears no longer nec as of 2018-10-23.  Was required in Sept 2018 as workaround for https://github.com/iiab/iiab/issues/1139 that blocked install of Admin Console
 rm /etc/apt/sources.list.d/rpi-testing.list
 # Clears the cache of rpi/testing
 apt update
