ce7e4762f0c3e996ee887a64d30cdcbe79f1e1b5
George Hunt <georgejhunt@gmail.com>
2019-06-14 11:53:58 -0700

    add tileserver.php

--- roles/osm-vector-maps/files/tileserver.php
+++ roles/osm-vector-maps/files/tileserver.php
@@ -10,8 +10,7 @@
 global $config;
 $config['serverTitle'] = 'Maps hosted with TileServer-php v2.0';
 $config['availableFormats'] = array('png', 'jpg', 'jpeg', 'gif', 'webp', 'pbf', 'hybrid');
-$config['dataRoot'] = '/library/www/osm-vector-maps/viewer/tiles/';
-//$config['dataRoot'] = '';
+$config['dataRoot'] = '';
 //$config['template'] = 'template.php';
 //$config['baseUrls'] = array('t0.server.com', 't1.server.com');
 
@@ -74,16 +73,16 @@ class Server {
 
     //Get config from enviroment
     $envServerTitle = getenv('serverTitle');
-    if($envServerTitle !== false){
+    if($envServerTitle !== FALSE){
       $this->config['serverTitle'] = $envServerTitle;
     }
     $envBaseUrls = getenv('baseUrls');
-    if($envBaseUrls !== false){
+    if($envBaseUrls !== FALSE){
       $this->config['baseUrls'] = is_array($envBaseUrls) ?
               $envBaseUrls : explode(',', $envBaseUrls);
     }
     $envTemplate = getenv('template');
-    if($envBaseUrls !== false){
+    if($envBaseUrls !== FALSE){
       $this->config['template'] = $envTemplate;
     }
   }
@@ -121,7 +120,7 @@ class Server {
       $this->x = $params[1];
       $file = explode('.', $params[0]);
       $this->y = $file[0];
-      $this->ext = isset($file[1]) ? $file[1] : null;
+      $this->ext = isset($file[1]) ? $file[1] : NULL;
     }
   }
 
@@ -137,7 +136,7 @@ class Server {
         return $value;
       }
     }
-    return false;
+    return FALSE;
   }
 
   /**
@@ -147,9 +146,9 @@ class Server {
    */
   public function isDBLayer($layer) {
     if (is_file($this->config['dataRoot'] . $layer . '.mbtiles')) {
-      return true;
+      return TRUE;
     } else {
-      return false;
+      return FALSE;
     }
   }
 
@@ -160,9 +159,9 @@ class Server {
    */
   public function isFileLayer($layer) {
     if (is_dir($layer)) {
-      return true;
+      return TRUE;
     } else {
-      return false;
+      return FALSE;
     }
   }
 
@@ -324,9 +323,9 @@ class Server {
     header('Etag:' . $eTag);
     if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $lastModifiedTime ||
             @trim($_SERVER['HTTP_IF_NONE_MATCH']) == $eTag) {
-      return true;
+      return TRUE;
     } else {
-      return false;
+      return FALSE;
     }
   }
 
@@ -340,7 +339,7 @@ class Server {
    */
   public function renderTile($tileset, $z, $y, $x, $ext) {
     if ($this->isDBLayer($tileset)) {
-      if ($this->isModified($tileset) == true) {
+      if ($this->isModified($tileset) == TRUE) {
         header('Access-Control-Allow-Origin: *');
         header('HTTP/1.1 304 Not Modified');
         die;
@@ -355,17 +354,17 @@ class Server {
       }
       $result = $this->db->query('select tile_data as t from tiles where zoom_level=' . $z . ' and tile_column=' . $x . ' and tile_row=' . $y);
       $data = $result->fetchColumn();
-      if (!isset($data) || $data === false) {
+      if (!isset($data) || $data === FALSE) {
         //if tile doesn't exist
         //select scale of tile (for retina tiles)
         $result = $this->db->query('select value from metadata where name="scale"');
         $resultdata = $result->fetchColumn();
-        $scale = isset($resultdata) && $resultdata !== false ? $resultdata : 1;
+        $scale = isset($resultdata) && $resultdata !== FALSE ? $resultdata : 1;
         $this->getCleanTile($scale, $ext);
       } else {
         $result = $this->db->query('select value from metadata where name="format"');
         $resultdata = $result->fetchColumn();
-        $format = isset($resultdata) && $resultdata !== false ? $resultdata : 'png';
+        $format = isset($resultdata) && $resultdata !== FALSE ? $resultdata : 'png';
         if ($format == 'jpg') {
           $format = 'jpeg';
         }
@@ -381,11 +380,11 @@ class Server {
     } elseif ($this->isFileLayer($tileset)) {
       $name = './' . $tileset . '/' . $z . '/' . $x . '/' . $y;
       $mime = 'image/';
-      if($ext != null){
+      if($ext != NULL){
         $name .= '.' . $ext;
       }
       if ($fp = @fopen($name, 'rb')) {
-        if($ext != null){
+        if($ext != NULL){
           $mime .= $ext;
         }else{
           //detect image type from file
@@ -422,6 +421,7 @@ class Server {
         header('Access-Control-Allow-Origin: *');
         header('HTTP/1.1 204 No Content');
         header('Content-Type: application/json; charset=utf-8');
+        echo '{"message":"Tile does not exist"}';
         break;
       case 'webp':
         header('Access-Control-Allow-Origin: *');
@@ -451,9 +451,9 @@ class Server {
    * @param integer $y
    * @param integer $x
    */
-  public function renderUTFGrid($tileset, $z, $y, $x, $flip = true) {
+  public function renderUTFGrid($tileset, $z, $y, $x, $flip = TRUE) {
     if ($this->isDBLayer($tileset)) {
-      if ($this->isModified($tileset) == true) {
+      if ($this->isModified($tileset) == TRUE) {
         header('HTTP/1.1 304 Not Modified');
       }
       if ($flip) {
@@ -467,7 +467,7 @@ class Server {
         $result = $this->db->query($query);
         $data = $result->fetch(PDO::FETCH_ASSOC);
 
-        if ($data !== false) {
+        if ($data !== FALSE) {
           $grid = gzuncompress($data['grid']);
           $grid = substr(trim($grid), 0, -1);
 
@@ -757,7 +757,7 @@ class Wmts extends Server {
    */
   public function get() {
     $request = $this->getGlobal('Request');
-    if ($request !== false && $request == 'gettile') {
+    if ($request !== FALSE && $request == 'gettile') {
       $this->getTile();
     } else {
       parent::setDatasets();
@@ -1080,7 +1080,7 @@ class Wmts extends Server {
   public function getTile() {
     $request = $this->getGlobal('Request');
     if ($request) {
-      if (strpos('/', $_GET['Format']) !== false) {
+      if (strpos('/', $_GET['Format']) !== FALSE) {
         $format = explode('/', $_GET['Format']);
         $format = $format[1];
       } else {
@@ -1293,7 +1293,7 @@ class Router {
       if (!isset($config['baseUrls'][0])) {
         $config['baseUrls'][0] = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
       }
-      if (strpos($_SERVER['REQUEST_URI'], '=') != false) {
+      if (strpos($_SERVER['REQUEST_URI'], '=') != FALSE) {
         $kvp = explode('=', $_SERVER['REQUEST_URI']);
         $_GET['callback'] = $kvp[1];
         $params[0] = 'index';
