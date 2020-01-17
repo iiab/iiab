924079dd951678df73700b84564bd0738c592d92
George Hunt <georgejhunt@gmail.com>
2020-01-16 20:24:54 -0800

    install dir.php

--- roles/osm-vector-maps/templates/dir.php
+++
@@ -1,6 +0,0 @@
-var files = <?php $out = array();
-foreach (glob('/library/www/osm-vector-maps.tiles/*') as $filename) {
-    $p = pathinfo($filenae);
-    $out[] = $p['filename'];
-}
-echo json_encode($out); ?>;
