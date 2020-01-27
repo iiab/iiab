64c937a7e303c6625e586c2b250a02ce3791dff2
George Hunt <georgejhunt@gmail.com>
2020-01-27 21:22:23 +0000

    osm-fixup receive url

--- roles/osm-vector-maps/files/osm-fixup.py
+++ roles/osm-vector-maps/files/osm-fixup.py
@@ -28,35 +28,28 @@ if len(sys.argv) != 3:
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Assemble Resources for Maps.")
-    parser.add_argument("map_url", help="The 'detail_url' field in mapcatalog.json.")
+    parser.add_argument("map_url", help="The 'detail_url' field in regions.json.")
     parser.add_argument("configdir", help="Place to look for cmdsrv.conf")
     return parser.parse_args()
 
 def main():
-    global map_catalog
-    args = parse_args()
-    map_catalog = adm.get_map_catalog()
-    catalog = map_catalog['maps']
-    found_map = catalog.get(args.map_url,'')
-    if found_map == '':
-        print('Download URL not found in map-catalog.json: %s'%args.map_url)
-        sys.exit(1)
-
-    osm_tile = CONST.maps_working_dir + str(os.path.basename(CONST.maps_osm_url))
-    sat_tile = CONST.maps_working_dir + str(os.path.basename(CONST.maps_sat_url))
-    for present in glob.glob(CONST.maps_working_dir + '/*'):
-        if present == osm_tile:
-            if os.path.isfile(CONST.maps_downloads_dir + os.path.basename(osm_tile)):
-               os.remove(CONST.maps_downloads_dir + os.path.basename(osm_tile))
-            shutil.move(osm_tile,CONST.maps_downloads_dir)
-        elif present == sat_tile:
-            if os.path.isfile(CONST.maps_downloads_dir + os.path.basename(sat_tile)):
-                os.remove(CONST.maps_downloads_dir + os.path.basename(sat_tile))
-            shutil.move(sat_tile,CONST.maps_downloads_dir)
-        else:
-            if os.path.isfile(CONST.maps_viewer_dir + 'tiles/' + os.path.basename(present)):
-                os.remove(CONST.maps_viewer_dir + 'tiles/' + os.path.basename(present))
-            shutil.move(present,CONST.maps_viewer_dir + 'tiles')
+   global config
+   args = parse_args()
+   configfile = args.configdir + '/cmdsrv.conf'
+   with open(configfile,'r') as conf_fp:
+      try:
+         inp = json.loads(conf_fp.read())
+      except Exception as e:
+         print("cmdsrv.json parse error:%s"%e)
+         sys.exit(1)
+   config = inp['cmdsrv_conf']      
+   get_regions()
+   found_region = ''
+   for region in regions_json.keys():
+      if reions_json[region]['detail_url'] == args.map_url: found = True
+   if found_region == '':
+      print('Download URL not found: %s'%args.map_url)
+      sys.exit(1)
 
    osm_tile = config['maps_working_dir'] + str(os.path.basename(config['maps_osm_url']))
    sat_tile = config['maps_working_dir'] + str(os.path.basename(config['maps_sat_url']))
@@ -75,10 +68,15 @@ def main():
          shutil.move(found,config['maps_viewer_dir'] + 'tiles')
 
 
-    try:
-      adm.subproc_run("iiab-update-map", shell=True)
-    except:
-      print('iiab-updatee-map ERROR')
+   # create init.json which sets initial coords and zoom
+   init = {}
+   init['region'] = found_region
+   init['zoom'] = regions_json[found_region]['zoom'] 
+   init['center_lon'] = regions_json[found_region]['center_lon'] 
+   init['center_lat'] = regions_json[found_region]['center_lat'] 
+   init_fn = viewer_path + '/init.json'
+   with open(init_fn,'w') as init_fp:
+      init_fp.write(json.dumps(init,indent=2))
 
 if __name__ == '__main__':
    if adm_cons_installed:
