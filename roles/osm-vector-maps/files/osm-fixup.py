210d98f1a239e5a1c72636211e7836f100424e47
George Hunt <georgejhunt@gmail.com>
2020-01-29 08:25:09 -0800

    can use adm-lib.py

--- roles/osm-vector-maps/files/osm-fixup.py
+++ roles/osm-vector-maps/files/osm-fixup.py
@@ -11,6 +11,16 @@ except:
     adm_cons_installed = False
     pass
 
+import iiab.iiab_lib as iiab
+
+try:
+    import iiab.adm_lib as adm
+    import iiab.adm_const as CONST
+    adm_cons_installed = True
+except:
+    adm_cons_installed = False
+    pass
+
 import argparse
 import sys, os
 import json
@@ -20,10 +30,10 @@ import json
 
 # GLOBALS
 viewer_path = '/library/www/osm-vector-maps/viewer'
-catalog_path = '/etc/iiab'
+region_path = '/etc/iiab'
 
 if len(sys.argv) != 3:
-   print("Argument 1=map_url, 2=<location of cmdsrv.conf>")
+   print("Argument 1=map_url, 2=<location or cmdsrv.conf>")
    sys.exit(1)
 
 def parse_args():
@@ -33,50 +43,80 @@ def parse_args():
     return parser.parse_args()
 
 def main():
-   global config
-   args = parse_args()
-   configfile = args.configdir + '/cmdsrv.conf'
-   with open(configfile,'r') as conf_fp:
-      try:
-         inp = json.loads(conf_fp.read())
-      except Exception as e:
-         print("cmdsrv.json parse error:%s"%e)
-         sys.exit(1)
-   config = inp['cmdsrv_conf']      
-   get_regions()
-   found_region = ''
-   for region in regions_json.keys():
-      if regions_json[region]['detail_url'] == args.map_url: found_region = region
-   if found_region == '':
-      print('Download URL not found: %s'%args.map_url)
-      sys.exit(1)
+    global map_catalog
+    args = parse_args()
+    map_catalog = adm.get_map_catalog()
+    regions = map_catalog['regions']
+    found_region = adm.get_region_from_tile(args.map_url)
+    if found_region == '':
+        print('Download URL not found in regions.json: %s'%args.map_url)
+        sys.exit(1)
 
-   osm_tile = config['maps_working_dir'] + str(os.path.basename(config['maps_osm_url']))
-   sat_tile = config['maps_working_dir'] + str(os.path.basename(config['maps_sat_url']))
-   for found in glob.glob(config['maps_working_dir'] + '/*'):
-      if found == osm_tile:
-         if os.path.isfile(config['maps_downloads_dir'] + os.path.basename(osm_tile)):
-            os.remove(config['maps_downloads_dir'] + os.path.basename(osm_tile))
-         shutil.move(osm_tile,config['maps_downloads_dir'])
-      elif found == sat_tile:
-         if os.path.isfile(config['maps_downloads_dir'] + os.path.basename(sat_tile)):
-            os.remove(config['maps_downloads_dir'] + os.path.basename(sat_tile))
-         shutil.move(sat_tile,config['maps_downloads_dir'])
-      else:
-         if os.path.isfile(config['maps_viewer_dir'] + 'tiles/' + os.path.basename(found)):
-            os.remove(config['maps_viewer_dir'] + 'tiles/' + os.path.basename(found))
-         shutil.move(found,config['maps_viewer_dir'] + 'tiles')
+    osm_tile = CONST.maps_working_dir + str(os.path.basename(CONST.maps_osm_url))
+    sat_tile = CONST.maps_working_dir + str(os.path.basename(CONST.maps_sat_url))
+    for found in glob.glob(CONST.maps_working_dir + '/*'):
+        if found == osm_tile:
+            if os.path.isfile(CONST.maps_downloads_dir + os.path.basename(osm_tile)):
+               os.remove(CONST.maps_downloads_dir + os.path.basename(osm_tile))
+            shutil.move(osm_tile,CONST.maps_downloads_dir)
+        elif found == sat_tile:
+            if os.path.isfile(CONST.maps_downloads_dir + os.path.basename(sat_tile)):
+                os.remove(CONST.maps_downloads_dir + os.path.basename(sat_tile))
+            shutil.move(sat_tile,CONST.maps_downloads_dir)
+        else:
+            if os.path.isfile(CONST.maps_viewer_dir + 'tiles/' + os.path.basename(found)):
+                os.remove(CONST.maps_viewer_dir + 'tiles/' + os.path.basename(found))
+            shutil.move(found,CONST.maps_viewer_dir + 'tiles')
 
 
-   # create init.json which sets initial coords and zoom
-   init = {}
-   init['region'] = found_region
-   init['zoom'] = regions_json[found_region]['zoom'] 
-   init['center_lon'] = regions_json[found_region]['center_lon'] 
-   init['center_lat'] = regions_json[found_region]['center_lat'] 
-   init_fn = viewer_path + '/init.json'
-   with open(init_fn,'w') as init_fp:
-      init_fp.write(json.dumps(init,indent=2))
+    # create init.json which sets initial coords and zoom
+    init = {}
+    init['region'] = found_region
+    init['zoom'] = regions[found_region]['zoom'] 
+    init['center_lon'] = regions[found_region]['center_lon'] 
+    init['center_lat'] = regions[found_region]['center_lat'] 
+    init_fn = viewer_path + '/init.json'
+    with open(init_fn,'w') as init_fp:
+        init_fp.write(json.dumps(init,indent=2))
+
+    adm.get_map_catalog()
+    map_menu_def_list = adm.get_map_menu_defs()
+    previous_idx = adm.read_vector_map_idx()
+
+    installed_tiles = adm.get_installed_tiles()
+
+    adm.write_vector_map_idx(installed_tiles)
+
+    # For installed regions, check that a menu def exists, and it's on home page
+    for fname in installed_tiles:
+        region = adm.get_region_from_tile(fname)
+        '''
+        if region == 'maplist': # it is the splash page, display only if no others
+            menu_item_name = 'en-map_test'
+            map_item = { "perma_ref" : menu_item_name }
+            if len(installed_maps) == 1:
+                adm.update_menu_json(menu_item_name)
+                return
+        elif region not in adm.map_catalog['regions']:
+        '''
+        if region not in adm.map_catalog['regions'].keys():
+            print("Skipping unknown map " + fname)
+            continue
+        else:
+            map_item = adm.map_catalog['regions'][region]
+            menu_item_name = map_item['perma_ref']
+
+            if not (menu_item_name in map_menu_def_list):
+                print('Creating menu def for %s'%menu_item_name)
+                adm.create_map_menu_def(region, menu_item_name, map_item)
+        # if autoupdate allowed and this is a new region then add to home menu
+        if adm.fetch_menu_json_value('autoupdate_menu') and menu_item_name not in previous_idx:
+            print('Auto-update of menu items is enabled. Adding %s'%region)
+            adm.update_menu_json(menu_item_name)
+            # redirect from box/maps to an installed map rather than test page
+            with open(adm.CONST.map_doc_root + '/index.html','w') as fp:
+                outstr = """<head> \n<meta http-equiv="refresh" content="0; URL=/osm-vector-maps/en-osm-omt_%s " />\n</head>"""%fname
+                fp.write(outstr)
 
 if __name__ == '__main__':
    if adm_cons_installed:
