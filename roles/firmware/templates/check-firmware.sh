0a747b152967f1438d625424af876c9686869135
Jerry Vonau <jvonau3@gmail.com>
2020-08-02 16:19:24 -0500

    add clm_blob and adjust

--- roles/firmware/templates/check-firmware.sh
+++ roles/firmware/templates/check-firmware.sh
@@ -7,15 +7,25 @@ else
     echo "$FW_MODE"
     if ! $(diff -q /lib/firmware/brcm/brcmfmac43455-sdio.bin.iiab /lib/firmware/brcm/brcmfmac43455-sdio.bin); then
         cp /lib/firmware/brcm/brcmfmac43455-sdio.bin.iiab /lib/firmware/brcm/brcmfmac43455-sdio.bin
+        echo "replacing firmware"
         WARN=1
     fi
     if ! $(diff -q /lib/firmware/brcm/brcmfmac43430-sdio.bin.iiab /lib/firmware/brcm/brcmfmac43430-sdio.bin); then
         cp /lib/firmware/brcm/brcmfmac43430-sdio.bin.iiab /lib/firmware/brcm/brcmfmac43430-sdio.bin
+        echo "replacing firmware"
+        WARN=1
+    fi
+    if ! $(diff -q /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob.iiab /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob); then
+        cp /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob.iiab /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob
+        echo "replacing firmware"
         WARN=1
     fi
 fi
-if [ "$WARN" = "1" ]
+if [ "$WARN" = "1" ]; then
+    echo "You should reboot now"
     touch /.fw_replaced
+    #echo "rebooting..."
+    #reboot
 else
     rm /.fw_replaced
 fi
