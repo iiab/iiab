8d70a2decf78ad0eda2e3359bf64943f98485dbc
A Holt <holta@users.noreply.github.com>
2021-08-24 12:14:06 -0400

    iiab-firmware-warn.sh: Explain 'sudo rm /.fw_replaced'

--- roles/firmware/templates/iiab-firmware-warn.sh
+++ roles/firmware/templates/iiab-firmware-warn.sh
@@ -3,7 +3,7 @@
 if [ -f /.fw_replaced ]; then
     echo -e "\n \033[31;5mWiFi Firmware has been replaced, per iiab/iiab#823.\033[0m"
     if grep -q '^wifi_hotspot_capacity_rpi_fix:\s\+[fF]alse\b' /etc/iiab/local_vars.yml ; then
-        echo -e " \033[31;5mYou can delete /.fw_replaced if you want these warnings to stop.\033[0m\n"
+        echo -e " \033[31;5mRun 'sudo rm /.fw_replaced' if you want these warnings to stop.\033[0m\n"
     else
         echo -e " \033[31;5mReboot is required to activate.\033[0m\n"
     fi
