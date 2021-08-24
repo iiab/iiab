79ed242bdfc1ebf252d4f3c95b873fe92882a6a3
root <holta@users.noreply.github.com>
2021-08-24 15:22:08 -0400

    iiab-check-firmware & iiab-firmware-warn.sh colors more readable

--- roles/firmware/templates/iiab-firmware-warn.sh
+++ roles/firmware/templates/iiab-firmware-warn.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ -f /.fw_replaced ]; then
-    echo -e "\n \033[31;5mWiFi Firmware has been replaced, per iiab/iiab#823.\033[0m"
+    echo -e "\n \e[41;1mWiFi Firmware has been replaced, per iiab/iiab#823.\e[0m"
     if grep -q '^wifi_hotspot_capacity_rpi_fix:\s\+[fF]alse\b' /etc/iiab/local_vars.yml ; then
-        echo -e " \033[31;5mRun 'sudo rm /.fw_replaced' if you want these warnings to stop.\033[0m\n"
+        echo -e " \e[100;1mIf you want these warnings to stop, run:\e[0m"
+        echo
+        echo -e " \e[100;1msudo rm /.fw_replaced\e[0m\n"
     else
-        echo -e " \033[31;5mReboot is required to activate.\033[0m\n"
+        echo -e " \e[41;1mReboot is required to activate.\e[0m\n"
     fi
 fi
