e7d51c2d7a79bfb76d5c1919b2731e24b9d68e5e
A Holt <holta@users.noreply.github.com>
2021-08-24 12:08:57 -0400

    iiab-firmware-warn.sh: Turn off warning tip, if wifi_hotspot_capacity_rpi_fix changed

--- roles/firmware/templates/iiab-firmware-warn.sh
+++ roles/firmware/templates/iiab-firmware-warn.sh
@@ -2,5 +2,9 @@
 
 if [ -f /.fw_replaced ]; then
     echo -e "\n \033[31;5mWiFi Firmware has been replaced, per iiab/iiab#823.\033[0m"
-    echo -e " \033[31;5mReboot is required to activate.\033[0m\n"
+    if grep -q '^wifi_hotspot_capacity_rpi_fix:\s\+[fF]alse\b' /etc/iiab/local_vars.yml ; then
+        echo -e " \033[31;5mYou can delete /.fw_replaced if you want these warnings to stop.\033[0m\n"
+    else
+        echo -e " \033[31;5mReboot is required to activate.\033[0m\n"
+    fi
 fi
