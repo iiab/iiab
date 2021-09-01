ecf6a9ec7d6e33dee7f26b01b7872a7bcbae5d72
George Hunt <georgejhunt@gmail.com>
2021-09-01 01:57:31 +0100

    use wild card to match any python

--- roles/jupyterhub/templates/patch_await.sh
+++ roles/jupyterhub/templates/patch_await.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 # add await to asyncio change password function
 
-cat {{ jupyterhub_venv }}/lib/python3.7/site-packages/firstuseauthenticator/firstuseauthenticator.py |grep 'await self.render'
+cat {{ jupyterhub_venv }}/lib/*/site-packages/firstuseauthenticator/firstuseauthenticator.py |grep 'await self.render'
 if [ $? -ne 0 ];then
    echo Updating file
-   sed -i -e's/self.render/await self.render/' {{ jupyterhub_venv }}/lib/python3.7/site-packages/firstuseauthenticator/firstuseauthenticator.py 
+   sed -i -e's/self.render/await self.render/' {{ jupyterhub_venv }}/lib/*/site-packages/firstuseauthenticator/firstuseauthenticator.py 
 else
    echo Patch already applied. Skipping. . .
 fi
