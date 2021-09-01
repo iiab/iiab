a4ffe5556b268577871cfe9a53358d054aa03846
George Hunt <georgejhunt@gmail.com>
2021-09-01 00:49:49 +0000

    softcode jupyterhub location

--- roles/jupyterhub/templates/patch-http-warning.sh
+++ roles/jupyterhub/templates/patch-http-warning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 # Do not enable the  warning about the insecurity of http protocol
 
-   sed -i -e's/if (window\.location.*/if (false) {/' /opt/iiab/jupyterhub/share/jupyterhub/templates/login.html
+   sed -i -e's/if (window\.location.*/if (false) {/' {{ jupyterhub_venv }}/share/jupyterhub/templates/login.html
