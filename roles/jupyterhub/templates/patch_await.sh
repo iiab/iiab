03c2b5d6e0eb5e391ec933cd44dbc71b4ab62828
George Hunt <georgejhunt@gmail.com>
2021-09-02 19:30:41 +0100

    lowercase username

--- roles/jupyterhub/templates/patch_await.sh
+++ roles/jupyterhub/templates/patch_await.sh
@@ -4,9 +4,16 @@
 SITE_PACKAGES=$({{ jupyterhub_venv }}/bin/getsite.py)
 cat $SITE_PACKAGES/firstuseauthenticator/firstuseauthenticator.py |grep 'await self.render'
 if [ $? -ne 0 ];then
-   echo Updating file
+   echo Updating to \"await self.render\"
    sed -i -e's/self.render/await self.render/' $SITE_PACKAGES/firstuseauthenticator/firstuseauthenticator.py 
 else
-   echo Patch already applied. Skipping. . .
+   echo Await patch already applied. Skipping. . .
+fi
+cat $SITE_PACKAGES/firstuseauthenticator/firstuseauthenticator.py |grep data['data'].lower()'
+if [ $? -ne 0 ];then
+	echo Updating to data['username'].lower()
+	sed -i -e's/data['username']/data['username'].lower()/' $SITE_PACKAGES/firstuseauthenticator/firstuseauthenticator.py
+else
+	echo username.lower() patch already applied. Skipping. . .
 fi
    
