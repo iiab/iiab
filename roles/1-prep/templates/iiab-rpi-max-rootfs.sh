e7b201e84cc5852fef6123a49999c2053cc5b877
A Holt <holta@users.noreply.github.com>
2022-02-18 13:46:46 -0500

    Further clarify 1-prep/templates/iiab-rpi-max-rootfs.sh

--- roles/1-prep/templates/iiab-rpi-max-rootfs.sh
+++ roles/1-prep/templates/iiab-rpi-max-rootfs.sh
@@ -13,20 +13,24 @@ if [ -f /.resize-rootfs ]; then
     if [ -x /usr/bin/raspi-config ]; then
         # 2022-02-17: Works in many more situations, e.g. with USB disks (not
         # just microSD cards).  IF ONLY THIS ALSO WORKED ON Ubuntu/Mint/etc !
+
+        # Uses do_expand_rootfs() from:
+        # https://github.com/RPi-Distro/raspi-config/blob/master/raspi-config
         raspi-config --expand-rootfs
     else
-        # 2022-02-17 PR #3121: This 2-line explanation appears stale...
-        # Assumes SD card style partition name like <device>p<partition number>
-        # Only works on F22 + where resizepart command exists
+        # ASSUMES SD CARD STYLE PARTITION NAME LIKE <device>p<partition number>
+        # e.g. /dev/mmcblk0p2 mounts at /           (typical RasPiOS microSD)
+        # BUT  /dev/sda2      mounts at /media/usb1 (typical RasPiOS USB boot
+        #                                            disk WON'T WORK BELOW!)
 
         # Calculate root partition
-        root_part=`lsblk -aP -o NAME,MOUNTPOINT | grep 'MOUNTPOINT="/"' | awk -F\" '{ print $2 }'`
-        root_dev=${root_part:0:-2}
+        root_part=`lsblk -aP -o NAME,MOUNTPOINT | grep 'MOUNTPOINT="/"' | awk -F\" '{ print $2 }'`    # e.g. mmcblk0p2
+        root_dev=${root_part:0:-2}    # e.g. mmcblk0
         # bash substring expansion: "negative offset [below, but not above]
         # must be separated from the colon by at least one space to avoid
         # being confused with the ‘:-’ expansion"
         # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
-        root_part_no=${root_part: -1}
+        root_part_no=${root_part: -1}    # e.g. 2
 
         # Resize partition
         growpart /dev/$root_dev $root_part_no
