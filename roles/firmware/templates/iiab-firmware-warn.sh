a2cbfc45d6f558e493272d4d8fb93c91b2a4275e
A Holt <holta@users.noreply.github.com>
2022-07-16 14:26:34 -0400

    firmware/templates/iiab-firmware-warn.sh: Suggest reboot

--- roles/firmware/templates/iiab-firmware-warn.sh
+++ roles/firmware/templates/iiab-firmware-warn.sh
@@ -2,9 +2,8 @@
 
 if [ -f /tmp/.fw_modified ]; then
     echo -e "\n\e[41;1mWiFi Firmware link(s) modified, per iiab/iiab#2853: PLEASE REBOOT!\e[0m"
-    # /tmp should be auto cleaned with a reboot
-    #echo
-    #echo -e "If you want this warning to stop, run: sudo rm /tmp/.fw_modified\n"
+    echo
+    echo -e "If you want this warning to stop, reboot to remove /tmp/.fw_modified\n"
 fi
 
 # \e[1m = bright white    \e[100;1m = bright white, on gray    \n\e[41;1m = bright white, on red
