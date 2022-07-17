e6155e6a864c9cc8d5710d889b008b57b0a706ee
Jerry Vonau <jvonau3@gmail.com>
2022-07-17 15:15:56 -0500

    netwarn - with reboot question

--- scripts/netwarn.sh
+++ scripts/netwarn.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
 if ! [ -f /etc/iiab/install-flags/iiab-network-complete ]; then
-    zenity --question --text="You need to provision the network. Ensure you have your upstream internet active first if needed. You will be prompted for your password. You should REBOOT afterwards, do you want to Proceed?"
+    zenity --question --width=200 --text="You need to provision the network. Ensure you have your upstream internet active first. You might be prompted for your password. Should you not want to provision the network at this time just click NO"
     rc=$?
     if [ $rc == "1" ]; then
-        exit 1
+        exit 0
     fi
     x-terminal-emulator -e /usr/local/bin/iiab-network
+    rc=$?
+    if [ $rc == "1" ]; then
+        zenity --question --width=200 --text="Network exited with error, please review /opt/iiab/iiab/iiab-network.log"
+        exit 1
+    fi
+    zenity --question --width=200 --text="A REBOOT is recommended, would you like to REBOOT now?"
+    rc=$?
+    if [ $rc == "1" ]; then
+        exit 0
+    fi
+    x-terminal-emulator -e /usr/sbin/reboot
 fi
