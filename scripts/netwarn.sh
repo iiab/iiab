4f05d638fb39c700bd2208406c96df0777715877
root <holta@users.noreply.github.com>
2022-07-21 14:02:14 -0400

    scripts/netwarn.sh: Cleaner pop-ups & #!/bin/sh in-line explanation

--- scripts/netwarn.sh
+++ scripts/netwarn.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
+
+# Some OS's like Ubuntu with LightDM *IGNORE* the above shebang line when this
+# script is invcked from /usr/share/mate/autostart/netwarn-iiab-network.desktop
+#
+# WHAT HAPPENS: sh (dash) NOT BASH will always be run!  As confirmed by:
+#
+# ps -p $$    # Whereas 'echo $SHELL' DOES NOT show the actual running shell!
+#
+# RECAP: We hard-code the above '#!/bin/sh' for uniformity across all distros.
+
 if [ -f /etc/iiab/install-flags/iiab-network-complete ]; then
     exit
 fi
 
-zenity --question --width=350 --text="IIAB needs to configure networking:\n\n► Internet must be live before you begin.\n►You might be prompted for your password.\n\nContinue?"
-case $? in
-    -1|5)
-        exit 1
-        ;;
-
-    1)
-        exit 0
-        ;;
-
-    0)
-        x-terminal-emulator -e /usr/local/bin/iiab-network
-        ;;
-esac
-
-if [ "$?" = "1" ]; then
-    zenity --warning --width=350 --text="iiab-network exited with error.\n\nPlease review /opt/iiab/iiab/iiab-network.log"
-    exit 1
+zenity --question --width=360 --text="IIAB needs to configure networking:\n\n► Internet must be live before you begin.\n►You might be prompted for your password.\n\nContinue?  (This can take 2-3 minutes)"
+rc=$?
+if [ "$rc" != "0" ]; then
+    exit $rc
 fi
 
-zenity --question --width=350 --text="iiab-network complete.\n\nWould you like to REBOOT now? (Recommended)"
+x-terminal-emulator -e /usr/local/bin/iiab-network
+rc=$?
+if [ "$rc" != "0" ]; then
+    zenity --warning --width=360 --text="iiab-network exited with error: $rc\n\nPlease review /opt/iiab/iiab/iiab-network.log"
+    exit $rc
+fi
+
+zenity --question --width=360 --text="iiab-network complete.\n\nWould you like to REBOOT now?  (Recommended)"
 if [ "$?" = "0" ]; then
     x-terminal-emulator -e "sudo reboot"
 fi
