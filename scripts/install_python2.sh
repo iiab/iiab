33f00a08535f905595a01d56dcfb865b7319c476
Jerry Vonau <jvonau3@gmail.com>
2023-04-06 17:15:33 -0500

    armhf

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -47,12 +47,24 @@ case $ARCH in
         apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
         ;;
     "armhf")
+        wget http://archive.raspberrypi.org/debian/pool/main/o/openssl/libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
+        apt install ./libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
+
+        wget http://raspbian.raspberrypi.org/raspbian/pool/main/libf/libffi/libffi7_3.3-6_armhf.deb
+        apt install ./libffi7_3.3-6_armhf.deb
+
+
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-13.2_armhf.deb
         apt install ./libpython2.7-minimal_2.7.18-13.2_armhf.deb
+
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-13.2_armhf.deb
         apt install ./libpython2.7-stdlib_2.7.18-13.2_armhf.deb
+
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/python2.7-minimal_2.7.18-13.2_armhf.deb
         apt install ./python2.7-minimal_2.7.18-13.2_armhf.deb
+
+        wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/python2.7-minimal_2.7.18-13.2_armhf.deb
+        apt install ./python2.7_2.7.18-13.2_armhf.deb
         ;;
 esac
 rm *.deb
