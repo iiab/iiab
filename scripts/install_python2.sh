af7a646002e52c5f0439a34268a725ff3391bf6b
Jerry Vonau <jvonau3@gmail.com>
2023-04-06 16:17:44 -0500

    amd64

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# https://packages.debian.org/bullseye/libpython2.7-stdlib
 ARCH=$(dpkg --print-architecture)
+
+apt -y install virtualenv
+apt -y install mime-support #transitional package
+#apt -y install libffi8
+
 cd /tmp
 case $ARCH in
     "arm64")
@@ -11,12 +17,23 @@ case $ARCH in
         apt install ./python2.7-minimal_2.7.18-8_arm64.deb
         ;;
     "amd64")
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_amd64.deb
-        apt install ./libpython2.7-minimal_2.7.18-8_amd64.deb
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_amd64.deb
-        apt install ./libpython2.7-stdlib_2.7.18-8_amd64.deb
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_amd64.deb
-        apt install ./python2.7-minimal_2.7.18-8_amd64.deb
+        wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
+        apt install ./libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
+
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/libf/libffi7/libffi7_3.3-5ubuntu1_amd64.deb
+        apt install ./libffi7_3.3-5ubuntu1_amd64.deb
+
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        apt install ./libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+        apt install ./libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        apt install ./python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+
+        wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
+        apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
         ;;
     "armhf")
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-13.2_armhf.deb
@@ -28,4 +45,3 @@ case $ARCH in
         ;;
 esac
 rm *.deb
-apt -y install virtualenv
