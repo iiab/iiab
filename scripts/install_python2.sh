9880ed1d7651a80c1e8a2ea270bf29cae54ba1cb
Jerry Vonau <jvonau3@gmail.com>
2023-04-05 21:00:39 -0500

    .so files2

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -5,20 +5,26 @@ case $ARCH in
     "arm64")
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_arm64.deb
         apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
+        apt install ./python2.7-stdlib_2.7.18-8_arm64.deb
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
         apt install ./python2.7-minimal_2.7.18-8_arm64.deb
         ;;
     "amd64")
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_amd64.deb
         apt install ./libpython2.7-minimal_2.7.18-8_amd64.deb
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_amd64.deb
+        apt install ./libpython2.7-stdlib_2.7.18-8_amd64.deb
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_amd64.deb
         apt install ./python2.7-minimal_2.7.18-8_amd64.deb
         ;;
     "armhf")
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-13.2_armhf.deb
         apt install ./libpython2.7-minimal_2.7.18-13.2_armhf.deb
+        wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-13.2_armhf.deb
+        apt install ./libpython2.7-stdlib_2.7.18-13.2_armhf.deb
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/python2.7-minimal_2.7.18-13.2_armhf.deb
-        apt install ./libpython2.7-minimal_2.7.18-13.2_armhf.deb
+        apt install ./python2.7-minimal_2.7.18-13.2_armhf.deb
         ;;
 esac
 rm *.deb
