5eac4ea24a9f5e91d66a00250a5f28fa3a1b2096
Jerry Vonau <jvonau3@gmail.com>
2023-04-08 02:49:06 -0500

    typo and layout

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -19,7 +19,7 @@ case $ARCH in
         apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
 
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
-        apt install ./python2.7-stdlib_2.7.18-8_arm64.deb
+        apt install ./libpython2.7-stdlib_2.7.18-8_arm64.deb
 
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
         apt install ./python2.7-minimal_2.7.18-8_arm64.deb
@@ -27,6 +27,7 @@ case $ARCH in
         wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8_arm64.deb
         apt install ./python2.7_2.7.18-8_arm64.deb
         ;;
+
     "amd64")
         wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
         apt install ./libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
@@ -46,13 +47,13 @@ case $ARCH in
         wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
         apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
         ;;
+
     "armhf")
         wget http://archive.raspberrypi.org/debian/pool/main/o/openssl/libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
         apt install ./libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
 
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/libf/libffi/libffi7_3.3-6_armhf.deb
         apt install ./libffi7_3.3-6_armhf.deb
-
 
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-13.2_armhf.deb
         apt install ./libpython2.7-minimal_2.7.18-13.2_armhf.deb
