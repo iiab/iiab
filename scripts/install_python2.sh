1563e71f8415bec1cd29893cc7002cb481be746f
Jerry Vonau <jvonau3@gmail.com>
2023-04-09 20:45:32 -0500

    kalite - libssl1.1

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -9,9 +9,6 @@ apt -y install mime-support #transitional package
 cd /tmp
 case $ARCH in
     "arm64")
-        wget http://archive.raspberrypi.org/debian/pool/main/o/openssl/libssl1.1_1.1.1n-0+deb11u4+rpt1_arm64.deb
-        apt install ./libssl1.1_1.1.1n-0+deb11u4+rpt1_arm64.deb
-
         wget http://ftp.debian.org/debian/pool/main/libf/libffi/libffi7_3.3-6_arm64.deb
         apt install ./libffi7_3.3-6_arm64.deb
 
@@ -29,9 +26,6 @@ case $ARCH in
         ;;
 
     "amd64")
-        wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
-        apt install ./libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
-
         wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/libf/libffi7/libffi7_3.3-5ubuntu1_amd64.deb
         apt install ./libffi7_3.3-5ubuntu1_amd64.deb
 
@@ -49,9 +43,6 @@ case $ARCH in
         ;;
 
     "armhf")
-        wget http://archive.raspberrypi.org/debian/pool/main/o/openssl/libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
-        apt install ./libssl1.1_1.1.1n-0+deb11u4+rpt1_armhf.deb
-
         wget http://raspbian.raspberrypi.org/raspbian/pool/main/libf/libffi/libffi7_3.3-6_armhf.deb
         apt install ./libffi7_3.3-6_armhf.deb
 
