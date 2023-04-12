6e08ff35ebbd763d4ffde111babec8fa7a49c5de
Jerry Vonau <jvonau3@gmail.com>
2023-04-11 22:21:01 -0500

    switch back to explicit dependencies

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -7,54 +7,50 @@ ARCH=$(dpkg --print-architecture)
 
 apt -y install virtualenv
 # https://github.com/iiab/iiab/pull/3535#issuecomment-1503626474
-apt -y install media-types
+apt -y install media-types libffi8 libssl3
 
 # libpython2.7-stdlib from ubuntu-22.04 used in amd64|arm64|armhf is compiled against libssl3 and libffi8
 # `apt info libpython2.7-stdlib`
 cd /tmp
 case $ARCH in
-    #"amd64")
-        #wget http://mirrors.edge.kernel.org/ubuntu/pool/main/libf/libffi/libffi8_3.4.2-4_amd64.deb
-        #apt install ./libffi8_3.4.2-4_amd64.deb
+    "arm64")
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_arm64.deb
+        apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
 
-        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
-        #apt install ./libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
+        apt install ./libpython2.7-stdlib_2.7.18-8_arm64.deb
 
-        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
-        #apt install ./libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
+        apt install ./python2.7-minimal_2.7.18-8_arm64.deb
 
-        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
-        #apt install ./python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8_arm64.deb
+        apt install ./python2.7_2.7.18-8_arm64.deb
+        rm *.deb
+        ;;
 
-        #wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
-        #apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
-        #;;
+    "amd64")
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        apt install ./libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
 
-    #"arm64")
-        #wget http://ftp.debian.org/debian/pool/main/libf/libffi/libffi7_3.3-6_arm64.deb
-        #apt install ./libffi7_3.3-6_arm64.deb
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+        apt install ./libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
 
-        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_arm64.deb
-        #apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
+        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        apt install ./python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
 
-        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
-        #apt install ./libpython2.7-stdlib_2.7.18-8_arm64.deb
-
-        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
-        #apt install ./python2.7-minimal_2.7.18-8_arm64.deb
-
-        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8_arm64.deb
-        #apt install ./python2.7_2.7.18-8_arm64.deb
-        #;;
+        wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
+        apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
+        rm *.deb
+        ;;
 
 # trusted is used for Debian and RasPiOS as the keys would be missing for Ubuntu
-    "arm64"|"amd64")
-        apt -y install libffi8 libssl3
-        cat << EOF >> /etc/apt/sources.list.d/python2.list
-deb [trusted=yes] http://ports.ubuntu.com/ jammy main universe
-deb [trusted=yes] http://ports.ubuntu.com/ jammy-updates main universe
-EOF
-        ;;
+#    "arm64"|"amd64")
+#        apt -y install
+#        cat << EOF >> /etc/apt/sources.list.d/python2.list
+#deb [trusted=yes] http://ports.ubuntu.com/ jammy main universe
+#deb [trusted=yes] http://ports.ubuntu.com/ jammy-updates main universe
+#EOF
+#        ;;
 
     "armhf")
         #wget http://raspbian.raspberrypi.org/raspbian/pool/main/libf/libffi/libffi7_3.3-6_armhf.deb
@@ -75,16 +71,14 @@ EOF
 
 # armhf compile flags differ between RasPiOS and Ubuntu
         if ! [ -f /etc/rpi-issue ]; then
-            apt -y install libffi8 libssl3
             cat << EOF >> /etc/apt/sources.list.d/python2.list
 deb http://ports.ubuntu.com/ jammy main universe
 deb http://ports.ubuntu.com/ jammy-updates main universe
 EOF
         fi
+        apt update
+        apt -y install python2
+        rm /etc/apt/sources.list.d/python2.list || true
+        apt update
         ;;
 esac
-
-apt update
-apt -y install python2
-rm /etc/apt/sources.list.d/python2.list || true
-apt update
