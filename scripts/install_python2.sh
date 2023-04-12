e6c4170f7c817eb481dce2e53d1924b294729fb5
Jerry Vonau <jvonau3@gmail.com>
2023-04-11 23:13:25 -0500

    pending feedback keyring

--- scripts/install_python2.sh
+++ scripts/install_python2.sh
@@ -13,44 +13,44 @@ apt -y install media-types libffi8 libssl3
 # `apt info libpython2.7-stdlib`
 cd /tmp
 case $ARCH in
-    "amd64")
-        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
-        apt install ./libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+    #"amd64")
+        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        #apt install ./libpython2.7-minimal_2.7.18-13ubuntu2_amd64.deb
 
-        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
-        apt install ./libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
+        #apt install ./libpython2.7-stdlib_2.7.18-13ubuntu2_amd64.deb
 
-        wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
-        apt install ./python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        #wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
+        #apt install ./python2.7-minimal_2.7.18-13ubuntu2_amd64.deb
 
-        wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
-        apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
-        rm *.deb
+        #wget http://mirrors.kernel.org/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu2_amd64.deb
+        #apt install ./python2.7_2.7.18-13ubuntu2_amd64.deb
+        #rm *.deb
         ;;
 
-    "arm64")
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_arm64.deb
-        apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
+    #"arm64")
+        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-minimal_2.7.18-8_arm64.deb
+        #apt install ./libpython2.7-minimal_2.7.18-8_arm64.deb
 
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
-        apt install ./libpython2.7-stdlib_2.7.18-8_arm64.deb
+        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.18-8_arm64.deb
+        #apt install ./libpython2.7-stdlib_2.7.18-8_arm64.deb
 
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
-        apt install ./python2.7-minimal_2.7.18-8_arm64.deb
+        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.18-8_arm64.deb
+        #apt install ./python2.7-minimal_2.7.18-8_arm64.deb
 
-        wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8_arm64.deb
-        apt install ./python2.7_2.7.18-8_arm64.deb
-        rm *.deb
-        ;;
+        #wget http://ftp.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8_arm64.deb
+        #apt install ./python2.7_2.7.18-8_arm64.deb
+        #rm *.deb
+        #;;
 
 # trusted is used for Debian and RasPiOS as the keys would be missing for Ubuntu
-#    "arm64"|"amd64")
-#        apt -y install
-#        cat << EOF >> /etc/apt/sources.list.d/python2.list
-#deb [trusted=yes] http://ports.ubuntu.com/ jammy main universe
-#deb [trusted=yes] http://ports.ubuntu.com/ jammy-updates main universe
-#EOF
-#        ;;
+    "arm64"|"amd64")
+        apt -y install ubuntu-keyring
+        cat << EOF >> /etc/apt/sources.list.d/python2.list
+deb [trusted=yes] http://ports.ubuntu.com/ jammy main universe
+deb [trusted=yes] http://ports.ubuntu.com/ jammy-updates main universe
+EOF
+        ;;
 
     "armhf")
         #wget http://raspbian.raspberrypi.org/raspbian/pool/main/libf/libffi/libffi7_3.3-6_armhf.deb
