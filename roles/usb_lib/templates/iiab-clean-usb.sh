190d66409f0265c81548584e9b52c28a5f6d1c48
Jerry Vonau <jvonau3@gmail.com>
2025-01-29 08:59:24 -0600

    Update roles/usb_lib/templates/iiab-clean-usb.sh
    to restore previous search-ability
    
    Co-authored-by: A Holt <holta@users.noreply.github.com>

--- roles/usb_lib/templates/iiab-clean-usb.sh
+++ roles/usb_lib/templates/iiab-clean-usb.sh
@@ -5,7 +5,7 @@ DEVICE=$(echo $@ | sed -s 's|-|/|')
 MNT_POINT=$(findmnt -n /$DEVICE | awk '{print $1}')
 CONTENT_LINK_USB=$(basename $MNT_POINT | awk '{print toupper($0)}')
 CONTENT_LINK="/library/www/html/local_content/$CONTENT_LINK_USB"
-logger -p user.notice -t "usb_lib (iiab-clean-usb.sh)" -- "Attempting to remove link $CONTENT_LINK."
+logger -p user.notice -t "usb_lib (iiab-clean-usb.sh)" -- "Attempting to remove link $CONTENT_LINK, as auto-created earlier by usbmount."
 
 if [ -L $CONTENT_LINK ]; then
   /bin/rm $CONTENT_LINK
