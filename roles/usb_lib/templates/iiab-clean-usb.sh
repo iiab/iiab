c935eb52bc4c67873386e23def03dc6915fc37dd
Jerry Vonau <jvonau3@gmail.com>
2025-02-15 11:12:15 -0600

    logging

--- roles/usb_lib/templates/iiab-clean-usb.sh
+++ roles/usb_lib/templates/iiab-clean-usb.sh
@@ -6,9 +6,9 @@ MNT_POINT=$(findmnt -no target $DEVICE)
 CONTENT_LINK_USB=$(basename $MNT_POINT | awk '{print toupper($0)}')
 CONTENT_LINK="/library/www/html/local_content/$CONTENT_LINK_USB"
 
-logger -t "usb_lib (iiab-clean-usb.sh)" "Attempting to remove symlink $CONTENT_LINK, as auto-created earlier by usbmount."
+logger -t "usbmount (iiab-clean-usb.sh)" "Attempting to remove symlink $CONTENT_LINK, as auto-created earlier by usb_lib."
 
 if [ -L $CONTENT_LINK ]; then
     /usr/bin/rm $CONTENT_LINK
-    logger -t "usb_lib (iiab-clean-usb.sh)" "Symlink $CONTENT_LINK removed, as auto-created earlier by usbmount."
+    logger -t "usbmount (iiab-clean-usb.sh)" "Symlink $CONTENT_LINK removed, as auto-created earlier by usb_lib."
 fi
