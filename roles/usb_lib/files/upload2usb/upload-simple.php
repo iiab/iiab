4728fa97b9c2d4da1572dc5b56b6a6f8e0e9faee
root <avni@users.noreply.github.com>
2025-03-25 01:32:02 -0400

    css changes: set max width for upload box; center upload box on mobile devices

--- roles/usb_lib/files/upload2usb/upload-simple.php
+++ roles/usb_lib/files/upload2usb/upload-simple.php
@@ -12,14 +12,25 @@ $file_count = getFileCount(getTargetFolderPath(0));
 ?>
 
     <style>
+    
+    div.container {
+      text-align:center;
+      max-width:350px;
+      float:right;
+    }
+    @media (max-width:768px) {
+      div.container {
+        float:none !important;
+	margin:auto;
+      }
+    }
     form {
       background-color: #ddd;
-      float:right;
       border-radius:.6rem;
       padding:.5rem;
     }
     input {
-      margin-right:-40px;
+      margin-right:-3rem;
       font-size:.75rem;
     }
     button {
@@ -45,11 +56,13 @@ $file_count = getFileCount(getTargetFolderPath(0));
 
     }
     </style>
+    <div class="container"> 
     <form action="/upload2usb/upload-file.php" id="upload2usb_form" method="post" enctype="multipart/form-data">
         <input type="file" name="uploaded_file" id="uploaded_file">
         <button class="btn btn-dark" name="submit" type="submit" style="width:80px;">Upload</button><br/>
         <span><?php echo $file_count ?> files uploaded today!</span>
     </form>
+    </div>
 
 
 
