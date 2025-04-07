9b5510d1ed135414d9b8c2da2b1ce3cab257f43a
root <avni@users.noreply.github.com>
2025-04-07 00:36:08 -0400

    Complete overlay positioning, design, and fading; Move CSS for simple form and overlay into separate file

--- roles/usb_lib/files/upload2usb/upload-simple.php
+++ roles/usb_lib/files/upload2usb/upload-simple.php
@@ -21,82 +21,15 @@ if (array_key_exists("query", $url_components)) {
 }
 ?>
 
+<link rel="stylesheet" href="/upload2usb/upload-simple.css">
+
 <script>
      function uploadFile() {
        document.getElementById("uploaded_file").click();
      };
 </script>
 
-<style>
-    div.container {
-      text-align:center;
-      width:260px;
-      max-width:350px;
-      float:right;
-      color: #fff;
-    }
-    @media (max-width:768px) {
-      div.container {
-        float:none !important;
-	margin:auto;
-      }
-    }
-    form {
-      background-color: #333;
-      border-radius:.3rem;
-      padding:.5rem;
-    }
-    button {
-      background-color: #ddd;
-      border: none;
-      border-radius:.3rem;
-      color: #333;
-      padding: .25rem .25rem;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size:.85rem;
-      font-weight:bold;
-      width:225px;
-    }
-    button:hover {
-      color: #666;
-    }
-    span {
-      display:block;
-      margin-top:5px;
-      text-align:center;
-      font-size:.8rem;
-    }
-
-    .overlay {
-      background-color:#000;
-      border:none;
-      border-radius:.3em;
-      padding:0.5rem;
-      color:#fff;
-      z-index:2;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin-top: -50px;
-      margin-left: -50px;
-      width: 400px;
-      height: 100px;
-      transition: 5s opacity, 5s visibility;
-      opacity: 1;
-      text-align:center;
-      font-size:.8rem;
-    }
-
-    .overlay.fade{
-       opacity: 0;
-       visibility: hidden;
-    }
-    
-</style>
-
-<div class="container">
+<div class="u2uform">
     <form action="/upload2usb/upload-file.php" id="upload2usb_form" method="post" enctype="multipart/form-data">
         <input type="file" name="uploaded_file" id="uploaded_file" onChange="this.form.submit();" style="display:none;">
 	<button name="upload_btn" type="button" onClick="uploadFile();">Upload to USB</button>
@@ -104,7 +37,14 @@ if (array_key_exists("query", $url_components)) {
     </form>
 </div>
 <?php if ($upload_msg != ''): ?>
-  <div class="overlay"><?php echo $upload_msg ?></div>
+  <div class="overlay fade-out">
+    <div class="overlay-inner">
+       <br/>
+       <a href="/usb/"><img class="mb-4" src="/upload2usb/uk-swing.png" alt="" width="75"></a> 
+       <p><span><?php echo $upload_msg ?></span></p>
+       <p><span><?php echo $file_count ?> files uploaded today!</span></p>
+    </div>
+  </div>
 <?php endif; ?>
 
 
