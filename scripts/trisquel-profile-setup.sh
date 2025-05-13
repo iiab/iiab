6358310ee2710539dff831f92192d848b1112873
A Holt <holta@users.noreply.github.com>
2025-05-13 12:59:44 -0400

    Lint/clean trisquel-profile-setup.sh

--- scripts/trisquel-profile-setup.sh
+++ scripts/trisquel-profile-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-   echo "This script must be run as root"
-   exit 1
+    echo "This script must be run as root"
+    exit 1
 fi
 
 . /etc/os-release
@@ -12,13 +12,14 @@ UPSTREAM_GPGKEY="/etc/apt/trusted.gpg.d/upstream_repo.gpg"
 PHP_APPARMOR=/etc/apparmor.d/php-fpm
 PHP_APPARMOR_DISABLED=/etc/apparmor.d/disable/php-fpm
 
-# Cleaning previos attempts
+# Cleaning previous attempts
 rm -f $UPSTREAM_REPO $UPSTREAM_PIN
 
-[ $VERSION_CODENAME = ecne ] && UPSTREAM=noble && REPO_GPGKEY="871920D1991BC93C" && VALID=1
+# There might be multiple such lines in future, e.g. if Trisquel 12 and 13 and both supported during a transitional period
+[ $VERSION_CODENAME = ecne ] && UPSTREAM=noble && REPO_GPGKEY="871920D1991BC93C" && VALID_TRISQUEL_VER=1
 
-if [ "$VALID" != 1 ]; then
-    echo "Not valid codename"
+if [ "$VALID_TRISQUEL_VER" != 1 ]; then
+    echo "IIAB supports only Trisquel 12 (Ecne) for now!"
     exit 1
 fi
 
@@ -26,8 +27,8 @@ fi
 apt-get -y install curl
 # Prevent apparmor profiles breaking server packages.
 ## php-fpm profile
-if [ -f "$PHP_APPARMOR_DISABLED" ];  then
-   echo "- php-fpm apparmor profile disabled, moving on."
+if [ -f "$PHP_APPARMOR_DISABLED" ]; then
+    echo "- php-fpm apparmor profile disabled, moving on."
 elif [ ! -f "$PHP_APPARMOR_DISABLED" ] && [ -f "$PHP_APPARMOR" ]; then
     ln -s /etc/apparmor.d/php-fpm /etc/apparmor.d/disable/
     apparmor_parser -R /etc/apparmor.d/php-fpm
