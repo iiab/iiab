e2766d799b7eafd4e796bac8793f49ef31d4660a
Jerry Vonau <jvonau3@gmail.com>
2025-08-30 12:29:33 -0500

    naming and remove script

---
+++ scripts/iiab-wired-slave-remove.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+echo -e "removing /etc/NetworkManager/conf/IIAB-Slave-*.conf \n"
+rm /etc/NetworkManager/conf/IIAB-Slave-*.conf
+echo -e "removing /etc/systemd/network/IIAB-Slave-*.network \n"
+rm /etc/systemd/network/IIAB-Slave-*.network
+echo -e "reboot to deactivate slaves devices. \n"
