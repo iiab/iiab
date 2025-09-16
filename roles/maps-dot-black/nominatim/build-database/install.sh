dfe0bcd83d7b250862785c15ee9447551cf39c55
Daniel Krol <orblivion@gmail.com>
2025-09-16 18:18:30 -0400

    Oops probably don't want to build the db before installing everything

--- roles/maps-dot-black/nominatim/build-database/install.sh
+++ roles/maps-dot-black/nominatim/build-database/install.sh
@@ -8,15 +8,9 @@ export USERNAME=nominatim
 export USERHOME=/srv/nominatim
 
 sudo apt-get update -qq
-sudo apt-get install -y osm2pgsql postgresql-postgis postgresql-postgis-scripts \
-                        pkg-config libicu-dev virtualenv
+sudo apt-get install -y osm2pgsql postgresql-postgis postgresql-postgis-scripts pkg-config libicu-dev virtualenv
 
 id nominatim || sudo useradd -d /srv/nominatim -s /bin/bash -m nominatim
-
-# Copy it each time in case we change it
-sudo cp nominatim-setup.prep /srv/nominatim/nominatim-setup.sh
-sudo chown nominatim:nominatim /srv/nominatim/nominatim-setup.sh
-sudo -u nominatim /srv/nominatim/nominatim-setup.sh
 
 sudo cp psql-nominatim.conf /etc/postgresql/*/main/conf.d/
 sudo service postgresql restart
@@ -26,3 +20,9 @@ sudo -u postgres dropuser --if-exists $USERNAME
 sudo -u postgres createuser -s        $USERNAME
 sudo -u postgres dropuser --if-exists www-data
 sudo -u postgres createuser           www-data
+
+# Copy it each time in case we change it
+sudo cp nominatim-setup.prep /srv/nominatim/nominatim-setup.sh
+sudo chown nominatim:nominatim /srv/nominatim/nominatim-setup.sh
+sudo -u nominatim /srv/nominatim/nominatim-setup.sh
+
