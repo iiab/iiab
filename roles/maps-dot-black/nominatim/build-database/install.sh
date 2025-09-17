77446188b69c49d8720fd946a51ef5d12c601cc7
Daniel Krol <orblivion@gmail.com>
2025-09-16 20:08:10 -0400

    Final tweaks, now it builds the sqlite file

--- roles/maps-dot-black/nominatim/build-database/install.sh
+++ roles/maps-dot-black/nominatim/build-database/install.sh
@@ -7,19 +7,18 @@ set -euo pipefail
 export USERNAME=nominatim
 export USERHOME=/srv/nominatim
 
-sudo apt-get update -qq
-sudo apt-get install -y osm2pgsql postgresql-postgis postgresql-postgis-scripts pkg-config libicu-dev virtualenv
+sudo apt update -qq
+sudo apt install -y osm2pgsql postgresql-postgis postgresql-postgis-scripts pkg-config libicu-dev virtualenv
+sudo apt install -y sqlite3 libsqlite3-mod-spatialite libspatialite8
 
 id nominatim || sudo useradd -d /srv/nominatim -s /bin/bash -m nominatim
 
 sudo cp psql-nominatim.conf /etc/postgresql/*/main/conf.d/
 sudo service postgresql restart
 
-# drop first so I can re-run this script
-sudo -u postgres dropuser --if-exists $USERNAME
-sudo -u postgres createuser -s        $USERNAME
-sudo -u postgres dropuser --if-exists www-data
-sudo -u postgres createuser           www-data
+# Try to silently fail. Hopefully it's because the user exists already.
+sudo -u postgres createuser -s        $USERNAME || echo "..."
+sudo -u postgres createuser           www-data || echo "..."
 
 # Copy it each time in case we change it
 sudo cp nominatim-setup.prep /srv/nominatim/nominatim-setup.sh
