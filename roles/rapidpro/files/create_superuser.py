2a6976a02d527948b553f7934d0757aac8a522fd
Blondel MONDESIR <blondel.md@gmail.com>
2025-11-08 13:28:35 -0500

    Adjust user creation setup

--- roles/rapidpro/files/create_superuser.py
+++ roles/rapidpro/files/create_superuser.py
@@ -1,19 +1,18 @@
 import os
-import django
-from django.conf import settings
 from django.contrib.auth import get_user_model
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temba.settings')
-django.setup()
+from django.core.exceptions import ObjectDoesNotExist
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'changeme')
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "changeme")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser '{username}' created successfully.")
-else:
-    print(f"Superuser '{username}' already exists.")
+try:
+    User.objects.get(email=email)
+    print("Superuser already exists.")
+except ObjectDoesNotExist:
+    u = User.objects.create_user(email=email, password=password)
+    u.is_superuser = True
+    u.is_staff = True
+    u.save()
+    print("Superuser created successfully!")
