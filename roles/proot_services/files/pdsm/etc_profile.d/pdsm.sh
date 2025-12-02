aaa924b609c7c51421b5d4df3c261de9c852394c
Ark74 <ark@switnet.org>
2025-12-02 14:20:12 -0600

    [pdsm] avoid running twice when services already excecuted.

--- roles/proot_services/files/pdsm/etc_profile.d/pdsm.sh
+++ roles/proot_services/files/pdsm/etc_profile.d/pdsm.sh
@@ -1,6 +1,22 @@
 # Auto-start psm services on login (proot)
+
+# Limit to interactive shells
+case $- in
+    *i*) ;;
+    *)  return ;;
+esac
+
 PSM_DIR="/usr/local/pdsm"
 ENABL="$PSM_DIR/services-enabled"
+PDSM_LOCK="$PSM_DIR/.psm-services.started"
+
+# If lock exists, no nothing.
+if [ -f "$PDSM_LOCK" ]; then
+    return
+fi
+
+# Create lock before starting anything.
+: >"$PDSM_LOCK"
 
 if [ -d "$ENABL" ]; then
   for svc in "$ENABL"/*; do
