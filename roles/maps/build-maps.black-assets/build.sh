f6c4dd0fc641f7cd9b5c6c21854d6cf47892a1f6
Daniel Krol <orblivion@gmail.com>
2026-01-02 11:08:54 -0500

    Build maps.black.js from a fork of the repo

--- roles/maps/build-maps.black-assets/build.sh
+++ roles/maps/build-maps.black-assets/build.sh
@@ -9,6 +9,11 @@ set -euo pipefail
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo bash - &&\
 sudo apt install -y nodejs
 
+git clone https://github.com/iiab/maps.black
+cd maps.black
+git checkout 12205728ceb0672682381088c6c1e7ad05fdc4f5
+cd client
+
 npm ci
 npm run prebuild
 npm run build-http
